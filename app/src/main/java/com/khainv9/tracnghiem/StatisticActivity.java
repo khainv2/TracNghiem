07345067670b5627eb3cfd63042138dd3d32a606
khai nguyen <khainv2@gmail.com>
2024-01-17 15:13:52 +0700

    Change bar chart to pie chart

--- app/src/main/java/com/khainv9/tracnghiem/StatisticActivity.java
+++ app/src/main/java/com/khainv9/tracnghiem/StatisticActivity.java
@@ -1,5 +1,6 @@
 package com.khainv9.tracnghiem;
 
+import android.graphics.Color;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
@@ -7,19 +8,29 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Range;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.Chart;
+import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.IValueFormatter;
 import com.github.mikephil.charting.formatter.ValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.interfaces.datasets.IPieDataSet;
+import com.github.mikephil.charting.utils.MPPointF;
 import com.khainv9.tracnghiem.adapter.DiemThiAdapter;
 import com.khainv9.tracnghiem.app.DatabaseManager;
 import com.khainv9.tracnghiem.models.ExamResult;
@@ -35,7 +46,16 @@ public class StatisticActivity extends AppCompatActivity {
     int examId;
     Toolbar toolbar;
     Examination examination;
-    BarChart chart;
+    PieChart chart;
+    public static final int[] COLORFUL_COLORS = {
+            Color.rgb(193, 37, 82),
+            Color.rgb(255, 102, 0),
+            Color.rgb(245, 199, 0),
+            Color.rgb(106, 150, 31),
+            Color.rgb(179, 100, 53),
+            Color.rgb(217, 80, 138), Color.rgb(254, 149, 7), Color.rgb(254, 247, 120),
+            Color.rgb(106, 167, 134), Color.rgb(53, 194, 209)
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +71,7 @@ public class StatisticActivity extends AppCompatActivity {
         examination = DatabaseManager.getExamination(examId);
 
         chart = findViewById(R.id.chart1);
-        chart.setDrawGridBackground(false);
+//        chart.setDrawGridBackground(false);
 
         TextView tv = findViewById(R.id.tv);
 
@@ -70,11 +90,7 @@ public class StatisticActivity extends AppCompatActivity {
         }
 
 
-
-//        tv.setText();
-//        tv.
-
-        // Find max
+        // Find min, max, avr
         double max = 0, min = 99999;
         double average = 0;
         for (ExamResult result: map.values()){
@@ -88,48 +104,78 @@ public class StatisticActivity extends AppCompatActivity {
 
         tv.setText("Điểm cao nhất: " + max + "\nĐiểm thấp nhất: " + min + "\nĐiểm trung bình: " + average + "\nSố lượng: " + map.size() + "\n");
 
-        if (max < 10){
-            max = 10;
+        List<Range<Double>> ranges = new ArrayList<>();
+        ranges.add(new Range<>(0.0, 1.0));
+        ranges.add(new Range<>(1.25, 3.25));
+        ranges.add(new Range<>(3.5, 4.75));
+        ranges.add(new Range<>(5.0, 6.75));
+        ranges.add(new Range<>(7.0, 8.0));
+        ranges.add(new Range<>(8.25, 10.0));
+
+        List<Integer> countPerRange = new ArrayList<>();
+        for (int i = 0; i < ranges.size(); i++) {
+            countPerRange.add(0);
         }
 
-        List<ExamResult> list = new ArrayList<>(map.values());
-        int[] histogram = new int[(int) Math.ceil(max) + 1];
-        for (ExamResult result: list){
-            histogram[(int) Math.floor(result.score)]++;
+        for (ExamResult result: map.values()){
+            for (int i = 0; i < ranges.size(); i++) {
+                if (ranges.get(i).contains(result.score)){
+                    countPerRange.set(i, countPerRange.get(i) + 1);
+                    break;
+                }
+            }
         }
 
+//        // Create fake data for all ranges
+//        countPerRange.set(0, 10);
+//        countPerRange.set(1, 5);
+//        countPerRange.set(2, 20);
+//        countPerRange.set(3, 7);
+//        countPerRange.set(4, 7);
+//        countPerRange.set(5, 6);
 
-        ArrayList<BarEntry> values = new ArrayList<>();
 
-        for (float i = 0.5f; i < 10.1f; i += 1f) {
-            values.add(new BarEntry(i, histogram[(int) Math.floor(i)]));
+
+        ArrayList<PieEntry> values = new ArrayList<>();
+        for (int i = 0; i < ranges.size(); i++) {
+            int count = countPerRange.get(i);
+            if (count == 0)
+                continue;
+            PieEntry entry = new PieEntry(countPerRange.get(i));
+            entry.setLabel("Điểm " + ranges.get(i).getLower() + " - " + ranges.get(i).getUpper());
+
+            values.add(entry);
         }
 
-        BarDataSet set1;
+        PieDataSet set1;
 
-        set1 = new BarDataSet(values, "Phổ điểm");
-
+        set1 = new PieDataSet(values, "Phổ điểm");
+        set1.setColors(COLORFUL_COLORS);
         set1.setDrawIcons(false);
 
-        ArrayList<IBarDataSet> dataSets = new ArrayList<>();
-        dataSets.add(set1);
-
-        BarData data = new BarData(dataSets);
+        PieData data = new PieData(set1);
         data.setValueTextSize(10f);
-        data.setBarWidth(0.6f);
+
         data.setValueFormatter(new ValueFormatter() {
             @Override
-            public String getFormattedValue(float value, AxisBase axis) {
-                return String.valueOf((int) value);
+            public String getPieLabel(float value, PieEntry pieEntry) {
+//                return "" + (int) value + " (" + String.format("%.2f", value / 55 * 100) + "%)";
+                return "" + (int) value + " (" + String.format("%.2f", value / map.size() * 100) + "%)";
             }
         });
         chart.setData(data);
-        chart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
-        chart.getXAxis().setLabelCount(11, true);
         chart.getDescription().setEnabled(false);
 
+        Legend l = chart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.VERTICAL);
+        l.setDrawInside(false);
+        l.setXEntrySpace(7f);
+        l.setYEntrySpace(0f);
+        l.setYOffset(0f);
         // Add spacing between bar
-        chart.setFitBars(false);
+//        chart.setFitBars(false);
 //        chart.disableScroll();
     }
 
